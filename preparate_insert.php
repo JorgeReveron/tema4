2887c4420bf2127976285216434b5ca10e37d172
JorgeReveron <jorgealexanderclases@gmail.com>
2023-11-14 19:26:28 +0000

    consultas preparadas y tansacciones

--- preparate_insert.php
+++ preparate_insert.php
@@ -1,9 +1,130 @@
 <?php
 require "conection.php";
+$newClientsString = '[{
+  "first_name": "Peta",
+  "birthday": "2023-08-11",
+  "cars": 10,
+  "vip": false
+}, {
+  "first_name": "Gabriel",
+  "birthday": "2023-02-18",
+  "cars": 6,
+  "vip": false
+}, {
+  "first_name": "Minor",
+  "birthday": "2023-10-13",
+  "cars": 6,
+  "vip": true
+}, {
+  "first_name": "Clementina",
+  "birthday": "2023-07-13",
+  "cars": 1,
+  "vip": false
+}, {
+  "first_name": "Tabbitha",
+  "birthday": "2023-03-12",
+  "cars": 5,
+  "vip": true
+}, {
+  "first_name": "Urban",
+  "birthday": "2023-10-29",
+  "cars": 3,
+  "vip": true
+}, {
+  "first_name": "Ethelind",
+  "birthday": "2023-10-16",
+  "cars": 8,
+  "vip": false
+}, {
+  "first_name": "Brockie",
+  "birthday": "2023-05-02",
+  "cars": 9,
+  "vip": false
+}, {
+  "first_name": "Franchot",
+  "birthday": "2023-02-18",
+  "cars": 4,
+  "vip": true
+}, {
+  "first_name": "Holly-anne",
+  "birthday": "2023-10-22",
+  "cars": 6,
+  "vip": true
+}, {
+  "first_name": "Rurik",
+  "birthday": "2022-12-13",
+  "cars": 7,
+  "vip": false
+}, {
+  "first_name": "Hazlett",
+  "birthday": "2023-08-05",
+  "cars": 7,
+  "vip": false
+}, {
+  "first_name": "Hayden",
+  "birthday": "2023-06-17",
+  "cars": 7,
+  "vip": false
+}, {
+  "first_name": "Katha",
+  "birthday": "2023-07-17",
+  "cars": 10,
+  "vip": false
+}, {
+  "first_name": "Minda",
+  "birthday": "2023-02-02",
+  "cars": 9,
+  "vip": true
+}, {
+  "first_name": "Valentine",
+  "birthday": "2023-08-14",
+  "cars": 4,
+  "vip": false
+}, {
+  "first_name": "Jamie",
+  "birthday": "2023-02-03",
+  "cars": 10,
+  "vip": false
+}, {
+  "first_name": "Theresa",
+  "birthday": "2023-07-18",
+  "cars": 4,
+  "vip": false
+}, {
+  "first_name": "Neils",
+  "birthday": "2022-12-16",
+  "cars": 4,
+  "vip": true
+}, {
+  "first_name": "Wilow",
+  "birthday": "2023-02-25",
+  "cars": 3,
+  "vip": true
+}]';
+
+$newClients = json_decode($newClientsString);
 
 $stmt = $link->stmt_init();
 
-$stmt->prepare();
+$stmt->prepare("INSERT INTO clients(first_name,birthday,cars,vip) VALUES (?,?,?,?)");
+
+$stmt->bind_param("ssis",$name,$birthday,$cars,$vip);
+
+foreach ($newClients as $client) {
+  $name = $client->first_name;
+  $birthday = $client->birthday;
+  $cars = $client->cars;
+  $vip = $client->vip;
+  $stmt->execute();
+}
+// $stmt->bind_param("ssis",$name,$birthday,$cars,$vip);
+// $name = "Alexander";
+// $birthday = "1999-09-09";
+// $cars = 3;
+// $vip = 1;
+
+// $stmt->execute();
+echo "insertado";
 
 $stmt->close();
 $link->close();
---
+++ transaccion2.php
@@ -0,0 +1,47 @@
+<?php
+require "conection.php";
+
+
+$stmt_query = $link->stmt_init();
+$stmt_query->prepare("SELECT id, first_name, cars FROM clients WHERE vip=1");
+$stmt_query->bind_result($id, $first_name, $cars);
+
+
+$stmt_insert = $link->stmt_init();
+$stmt_insert->prepare("INSERT INTO clientsVIP (name,cars) VALUES(?,?)");
+$stmt_insert->bind_param("si",$first_name,$cars);
+
+$stmt_delete = $link->stmt_init();
+$stmt_delete->prepare("DELETE FROM clients WHERE id=?");
+$stmt_delete->bind_param("i",$id);
+
+$link->autocommit(FALSE);
+$stmt_query->execute();
+
+while ($stmt_query->fetch()) {
+  // Comprobar si cars == 0 para anular la transaccion
+
+  if($cars == 0) {
+    echo "Se aborta porque el cliente con id=$id tiene 0 coches";
+    $link->rollback();
+    exit();
+  }
+
+  // Insertar en la nueva tabla
+
+  $stmt_insert->execute();
+  echo "<p>Se inserta el cliente $first_name con el numero de coches $cars</p>";
+
+  // Borrar de la tabla
+
+  $stmt_delete->execute();
+  echo "<p>Se ha borrado el cliente con el id = $id</p>";
+}
+if(!$link->commit()){
+  echo "Commit tansaction failed";
+  exit();
+} 
+
+$stmt_insert->close();
+$stmt_delete->close();
+$link->close();
